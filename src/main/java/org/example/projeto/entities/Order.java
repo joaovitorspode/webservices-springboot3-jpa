8b04fa0d5d804f9398e4ec15cd2c19fa0404d6b5
João Vitor Spode <joaospode.contato@gmail.com>
2024-08-14 10:33:37 -0300

    Enum OrderStatus

--- src/main/java/org/example/projeto/entities/Order.java
+++ src/main/java/org/example/projeto/entities/Order.java
@@ -3,6 +3,7 @@ package org.example.projeto.entities;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import org.example.projeto.entities.enums.OrderStatus;
 
 import java.io.Serializable;
 import java.time.Instant;
@@ -20,6 +21,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -27,9 +30,10 @@ public class Order implements Serializable {
 
     public Order() {}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -55,6 +59,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
